fc2a6dd1cbf32803fabb12ab155365da8c960ea1
tonnylyz <lyztonny@gmail.com>
2018-11-13 20:33:28 +0800

    Add commit hook

--- task/app.c
+++ task/app.c
@@ -1,17 +1,9 @@
 #include <uart.h>
 #include <ucos_ii.h>
+#include <app_cfg.h>
 
-char App_TaskStartStk[512u];
-
-void App_TaskStart(void *p_arg) {
-#if (OS_TASK_STAT_EN > 0u)
-    OSStatInit();                                               /* Determine CPU capacity                               */
-#endif
-
-#ifdef CPU_CFG_INT_DIS_MEAS_EN
-    CPU_IntDisMeasMaxCurReset();
-#endif
-    while (1) {                                          /* Task body, always written as an infinite loop.       */
+void App_Main() {
+    while (1) {
         uart_print_dec(1);
         int j = 0xffff;
         while (j--) {
@@ -20,33 +12,51 @@ void App_TaskStart(void *p_arg) {
     }
 }
 
-void App_TaskCreateHook(struct os_tcb *ptcb) {
+void App_TaskStart(void *p_arg) {
+    uart_puts("App_TaskStart entered\n");
 
+#if (OS_TASK_STAT_EN > 0u)
+    OSStatInit();                                               /* Determine CPU capacity                               */
+    uart_puts("OSStatInit done\n");
+#endif
+
+
+    App_Main();
 }
 
-void App_TaskDelHook(struct os_tcb *ptcb) {
+void App_TaskCreateHook(OS_TCB *ptcb) {
+    uart_puts("App_TaskCreateHook called\n");
+}
+
+void App_TaskDelHook(OS_TCB *ptcb) {
+    uart_puts("App_TaskDelHook called\n");
 
 }
 
 void App_TaskIdleHook(void) {
+    uart_puts("App_TaskIdleHook called\n");
 }
 
 void App_TaskStatHook(void) {
+    uart_puts("App_TaskStatHook called\n");
 }
 
-void App_TaskReturnHook(struct os_tcb *ptcb) {
+void App_TaskReturnHook(OS_TCB *ptcb) {
+    uart_puts("App_TaskReturnHook called\n");
 
-    (void) ptcb;
 }
 
 void App_TaskSwHook(void) {
+    uart_puts("App_TaskSwHook called\n");
 
 }
 
-void App_TCBInitHook(struct os_tcb *ptcb) {
+void App_TCBInitHook(OS_TCB *ptcb) {
+    uart_puts("App_TCBInitHook called\n");
 
 }
 
 void App_TimeTickHook(void) {
+    uart_puts("App_TimeTickHook called\n");
 
 }

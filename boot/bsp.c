5d8af8d6c8dfab5a31924430419181adcfcd9e06
tonnylyz <lyztonny@gmail.com>
2018-11-13 20:47:44 +0800

    Update bsp

--- boot/bsp.c
+++ boot/bsp.c
@@ -1,21 +1,25 @@
 #include <ucos_ii.h>
+#include <cpu_core.h>
 #include <uart.h>
 #include <timer.h>
 #include <types.h>
 
-#define  APP_CFG_TASK_START_PRIO                           2u
-#define  APP_CFG_TASK_START_STK_SIZE                     512u
+OS_STK App_TaskStartStk[APP_CFG_TASK_START_STK_SIZE];
 
-extern OS_STK App_TaskStartStk[APP_CFG_TASK_START_STK_SIZE];
 extern void App_TaskStart();
 
-void main() {
+int main() {
 
     INT8U      os_err;
 
     uart_init();
-    uart_puts("main\n");
-    uart_puts("BSP_PreInit finished\n");
+    uart_puts("OS Build: ");
+    uart_puts(__TIMESTAMP__);
+    uart_putc('\n');
+
+    uart_puts("uart_init finished\n");
+
+
 
     CPU_Init();                                                 /* Initialize the uC/CPU services                       */
 
@@ -27,38 +31,58 @@ void main() {
     CPU_IntDis();
 
     OSInit();                                                   /* Init uC/OS-II.                                       */
+    uart_puts("OSInit done\n");
 
-    uart_puts("will create task");
-    OSTaskCreateExt((void (*)(void *)) App_TaskStart,           /* Create the start task                                */
-                    (void           *) 0,
-                    (OS_STK         *)&App_TaskStartStk[APP_CFG_TASK_START_STK_SIZE - 1],
-                    (INT8U           ) APP_CFG_TASK_START_PRIO,
-                    (INT16U          ) APP_CFG_TASK_START_PRIO,
-                    (OS_STK         *)&App_TaskStartStk[0],
-                    (INT32U          ) APP_CFG_TASK_START_STK_SIZE,
-                    (void           *) 0,
-                    (INT16U          )(OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR));
-    uart_puts("created task\n");
-#if (OS_TASK_NAME_EN > 0u)
-    OSTaskNameSet(APP_CFG_TASK_START_PRIO, (INT8U *)"Start", &os_err);
-#endif
-    uart_puts("timer_init\n");
-    timer_init();
+    OSTaskCreate(App_TaskStart, (void *)0, &App_TaskStartStk[APP_CFG_TASK_START_STK_SIZE - 1], 0);
+    uart_puts("OSTaskCreate done\n");
+
+    CPU_IntEn();
+
     OSStart();                                                  /* Start multitasking (i.e. give control to uC/OS-II).  */
 
     while (1);
 }
 
 void OS_CPU_IntHandler (u32  src_id) {
-    uart_puts("OS_CPU_IntHandler");
+    uart_puts("OS_CPU_IntHandler called\n");
+
 }
 
 u32  CPU_TS_TmrRd ()
 {
+    uart_puts("CPU_TS_TmrRd called\n");
+    while (1);
     return 0;
 }
 
 void  CPU_TS_TmrInit ()
 {
-    uart_puts("CPU_TS_TmrInit");
+    uart_puts("CPU_TS_TmrInit called\n");
+    timer_init();
+}
+
+
+void cpu_undef_ins() {
+    uart_puts("cpu_undef_ins called\n");
+    while (1);
+}
+
+void cpu_svc() {
+    uart_puts("cpu_svc called\n");
+    while (1);
+}
+
+void cpu_prefetch_abort() {
+    uart_puts("cpu_prefetch_abort called\n");
+    while (1);
+}
+
+void cpu_data_abort() {
+    uart_puts("cpu_data_abort called\n");
+    while (1);
+}
+
+void cpu_fiq() {
+    uart_puts("cpu_fiq called\n");
+    while (1);
 }

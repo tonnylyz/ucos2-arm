3155b7b1b24960801066cbd2d2337d0e89ed21cb
tonnylyz <lyztonny@gmail.com>
2018-11-27 11:21:53 +0800

    Revert: Clean up

---
+++ platform/arm/os_cpu_cfg.h
@@ -0,0 +1,31 @@
+/*
+*********************************************************************************************************
+*                                                uC/OS-II
+*                                          The Real-Time Kernel
+*
+*
+*                           (c) Copyright 2009-2010; Micrium, Inc.; Weston, FL
+*                    All rights reserved.  Protected by international copyright laws.
+*
+*                                      Generic ARM CORTEX-A9 Port
+*                                            Configuration
+*
+* File      : OS_CPU.H
+* Version   : V2.92
+* By        : NB
+*
+* For       : ARM Cortex-A9 (ARMv7)
+* Mode      : ARM or Thumb
+* Toolchain : gcc
+*********************************************************************************************************
+*/
+
+#ifndef  OS_CPU_CFG_MODULE_PRESENT
+#define  OS_CPU_CFG_MODULE_PRESENT
+
+#define  OS_CPU_ARM_FP_EN                                 1
+
+#define  OS_CPU_CFG_ARM_LITTLE_ENDIAN                     1
+
+
+#endif

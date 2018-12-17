f9b707d595d527996739761cfdfbea6d29b4e050
tonnylyz <lyztonny@gmail.com>
2018-12-17 23:52:45 +0800

    Remove uart_print_*

--- platform/csp/uart.h
+++ platform/csp/uart.h
@@ -10,15 +10,10 @@
 
 #include "types.h"
 
+void uart1_init();
+
 void uart_putc(char c);
 
 void uart_puts(char *str);
-
-void uart_print_dec(uint32_t dec);
-
-void uart_print_bin(uint32_t bin);
-
-void uart_print_hex(uint32_t hex);
-
 
 #endif //UCOS2_UART_H
